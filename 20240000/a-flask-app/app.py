1cf70c793e3776018f8d3ee518393a3f929e0167
matteobescape <152966945+matteobescape@users.noreply.github.com>
2024-07-02 13:11:01 +0000

    cambiamento host

--- 20240000/a-flask-app/app.py
+++ 20240000/a-flask-app/app.py
@@ -61,7 +61,7 @@ def main():
     risposta = "nessuna risposta"
     try:
         connection = mysql.connector.connect(
-            host="its-rizzoli-idt-mysql.mysql.database.azure.com",
+            host="its-rizzoli-idt-mysql-82342.mysql.database.azure.com",
             user="psqladmin",
             passwd="H@Sh1CoR3!",
             database="ufs05db"
